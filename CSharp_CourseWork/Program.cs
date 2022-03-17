7ea219a4f19ce9c632374d6547a86931336d0105
Ivan Krylov <50463318+ivan1krulov@users.noreply.github.com>
2022-03-17 19:00:23 +0300

    Add project files.

---
+++ CSharp_CourseWork/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CSharp_Course_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        static void IncrementDecrement()
+        {
+            Console.WriteLine("Increments example");
+            Console.WriteLine("");
+
+            int x = 1;
+            int j = 0;
+
+            Console.WriteLine($"X state is {x}");
+            Console.WriteLine($"J state is {j}");
+            Console.WriteLine("");
+            j = x++;
+
+            Console.WriteLine($"Now J state is {j}");
+            Console.WriteLine($"Now X state is {x}");
+            Console.WriteLine("");
+
+            j = ++x;
+            Console.WriteLine($"And now X state is {x}");
+            Console.WriteLine($"And now J state is {j}");
+            Console.WriteLine("");
+        }
+    }
+}
