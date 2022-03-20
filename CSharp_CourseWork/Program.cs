22d488016b30809230b9c824f53defe3bea7eb14
Ivan Krylov <50463318+ivan1krulov@users.noreply.github.com>
2022-03-20 21:24:47 +0300

    Day work

--- CSharp_CourseWork/Program.cs
+++ CSharp_CourseWork/Program.cs
@@ -6,29 +6,31 @@ namespace CSharp_Course_Practice
     {
         static void Main(string[] args)
         {
+            string name = "John";
+            int age = 13;
+            Console.WriteLine("My name is {0} and my age {1}", name, age);
+            static void IncrementDecrement()
+            {
+                Console.WriteLine("Increments example");
+                Console.WriteLine("");
 
-        }
-        static void IncrementDecrement()
-        {
-            Console.WriteLine("Increments example");
-            Console.WriteLine("");
+                int x = 1;
+                int j = 0;
 
-            int x = 1;
-            int j = 0;
+                Console.WriteLine($"X state is {x}");
+                Console.WriteLine($"J state is {j}");
+                Console.WriteLine("");
+                j = x++;
 
-            Console.WriteLine($"X state is {x}");
-            Console.WriteLine($"J state is {j}");
-            Console.WriteLine("");
-            j = x++;
+                Console.WriteLine($"Now J state is {j}");
+                Console.WriteLine($"Now X state is {x}");
+                Console.WriteLine("");
 
-            Console.WriteLine($"Now J state is {j}");
-            Console.WriteLine($"Now X state is {x}");
-            Console.WriteLine("");
-
-            j = ++x;
-            Console.WriteLine($"And now X state is {x}");
-            Console.WriteLine($"And now J state is {j}");
-            Console.WriteLine("");
+                j = ++x;
+                Console.WriteLine($"And now X state is {x}");
+                Console.WriteLine($"And now J state is {j}");
+                Console.WriteLine("");
+            }
         }
     }
 }
